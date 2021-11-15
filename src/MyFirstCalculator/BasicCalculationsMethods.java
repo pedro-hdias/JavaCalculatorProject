481cf2505a715a2cb676fb937349ded677075338
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-15 16:26:04 -0300

    Basic calculator metods added.

---
+++ src/MyFirstCalculator/BasicCalculationsMethods.java
@@ -0,0 +1,19 @@
+package MyFirstCalculator;
+
+public class BasicCalculationsMethods {
+	public void Sum(int sum1, int sum2) {
+		System.out.println(sum1+sum2);
+	}
+	
+	public void Subtraction(int sub1, int sub2) {
+		System.out.println(sub1-sub2);
+	}
+	
+	public void Multiplication(int mult1, int mult2) {
+		System.out.println(mult1*mult2);
+	}
+	
+	public voidd Division(int div1, int div2) {
+		System.out.println(div1/div2);
+	}
+}
